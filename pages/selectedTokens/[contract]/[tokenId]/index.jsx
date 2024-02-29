760b1cc0ec07a1c5a477d0f3247c79a4567fed55
wuianski <wuiyeh@gmail.com>
2024-02-29 11:01:30 +0800

    change the way of fetching data in front page

--- pages/selectedTokens/[contract]/[tokenId]/index.jsx
+++ pages/selectedTokens/[contract]/[tokenId]/index.jsx
@@ -26,31 +26,44 @@ export default function TokenId({ data }) {
   );
 }
 
-/* This function gets called at build time */
-export async function getStaticPaths() {
-  /* Call an external API endpoint to get all tokens */
-  const [data] = await Promise.all([await MainnetAPI("/fa2tokens")]);
-  /* Get the paths we want to pre-render based on contract and tokenId */
-  const paths = data.tokens.map((token) => ({
-    params: {
-      contract: token.contract,
-      tokenId: token.tokenId.toString(),
-    },
-  }));
-  /* We'll pre-render only these paths at build time.  */
-  /* { fallback: false } means other routes should 404.  */
-  return {
-    paths,
-    fallback: false,
-  };
-}
+// /* This function gets called at build time */
+// export async function getStaticPaths() {
+//   /* Call an external API endpoint to get all tokens */
+//   const [data] = await Promise.all([await MainnetAPI("/fa2tokens?limit=24")]);
+//   /* Get the paths we want to pre-render based on contract and tokenId */
+//   const paths = data.tokens.map((token) => ({
+//     params: {
+//       contract: token.contract,
+//       tokenId: token.tokenId.toString(),
+//     },
+//   }));
+//   /* We'll pre-render only these paths at build time.  */
+//   /* { fallback: false } means other routes should 404.  */
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
 
-/* This also gets called at build time */
-export async function getStaticProps({ params }) {
-  // console.log(params)
+// /* This also gets called at build time */
+// export async function getStaticProps({ params }) {
+//   // console.log(params)
+//   const [data] = await Promise.all([
+//     await MainnetAPI(`/fa2tokens/${params.contract}/${params.tokenId}`),
+//   ]);
+//   /* Pass data to the page via props */
+//   return { props: { data } };
+// }
+
+export async function getServerSideProps(params) {
+  //   console.log(params.params.id);
   const [data] = await Promise.all([
-    await MainnetAPI(`/fa2tokens/${params.contract}/${params.tokenId}`),
+    await MainnetAPI(
+      `/fa2tokens/${params.params.contract}/${params.params.tokenId}`
+    ),
   ]);
-  /* Pass data to the page via props */
-  return { props: { data } };
+
+  return {
+    props: { data },
+  };
 }

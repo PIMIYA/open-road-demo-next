7c36588308d904198e711476a39ed13473edca09
19 <sorarize@gmail.com>
2024-04-08 15:06:33 +0800

    Add FeatureBox

---
+++ components/homepage/featureBox.jsx
@@ -0,0 +1,84 @@
+import parse from 'html-react-parser';
+import Box from "@mui/material/Box";
+import Image from "next/image";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
+
+import bg1 from "@/public/bubble1_bg.png";
+import bg2 from "@/public/bubble2_bg.png";
+import bg3 from "@/public/bubble3_bg.png";
+import bg4 from "@/public/bubble4_bg.png";
+
+const bgs = [bg1, bg2, bg3, bg4];
+
+export default function FeatureBox({ bgIndex, title, description }) {
+  description = parse(description.replaceAll('在場證明', '<strong>在場證明</strong>'));
+  const bg = bgs[bgIndex];
+
+  return (
+    <Box
+      sx={{
+        maxWidth: 600,
+        margin: '0 auto',
+        mb: 8,
+        ':nth-of-type(2n)': {
+          transform: { lg: 'translateX(60%)'},
+        },
+        ':nth-of-type(2n+1)': {
+          transform: { lg: 'translateX(-60%)'},
+        },
+        ':nth-of-type(n + 3)': {
+          mt: { lg: -30 },
+        }
+      }}
+    >
+      <Box
+        sx={{
+          maxHeight: 300,
+          height: { xs: '45vw', md: '25vw' },
+          background: '#fff',
+          position: 'relative',
+          borderRadius: '4vw',
+          border: '1px solid #000',
+          margin: '0 auto',
+        }}
+      >
+        <Image
+          src={bg}
+          alt={title}
+          style={{
+            width: 'auto',
+            height: '100%',
+            position: 'absolute',
+            left: '50%',
+            transform: 'translateX(-50%)',
+          }}
+        />
+      </Box>
+      <Box
+        sx={{
+          px: { xs: 2, md: 6},
+          py: { xs: 2, md: 4 },
+        }}
+      >
+        <Typography
+          component='h1'
+          variant='h5'
+          sx={{
+            fontWeight: 'bold',
+            background: '#d5ff7c',
+            display: 'inline-block',
+            mb: 2,
+          }}
+        >
+          {title}
+        </Typography>
+        <Typography
+          fontSize={18}
+        >
+          {description}
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -16,6 +16,8 @@ import { MainnetAPI } from "@/lib/api";
 /* Components */
 import SelectedTokenCardGrid from "@/components/selectedTokenCardGrid";
 import MyPagination from "@/components/myPagination";
+import FeatureBox from "@/components/homepage/featureBox";
+
 import { useState } from "react";
 import { paginate } from "@/lib/paginate";
 
@@ -33,7 +35,17 @@ export default function Home({ data }) {
   return (
     <>
       <Container maxWidth="lg">
-        <Box p={6} sx={{ textAlign: "center" }}>
+        <div>開路 KV</div>
+        <FeatureBox bgIndex={0} title="紀錄你的藝文活動！" description="每次參加活動，你都能拿到一份屬於該活動的在場證明。內容由創作者決定，可能是節目單、可能是練習的側拍、可能是海報主視覺。" />
+        <FeatureBox bgIndex={1} title="與創作者建立連結" description="透過在場證明，創作者將知道誰最常參與過他們的活動。<br />同時，唯有持有在場證明者，可以對活動發表評論。" />
+        <FeatureBox bgIndex={2} title="探索喜好" description="透過在場證明，所有人皆可以探索彼此的藝文活動路徑。身為參與者，你可能找到跟你喜好相近的同好；身為創作者，你可能找到將來的合作對象。" />
+        <FeatureBox bgIndex={3} title="保有匿名性的開放" description="所有資料都公開於鏈上，所有 ID 都是錢包地址，<br />借助 web3 的技術達到安全、匿名但真實的開放資料。" />
+        <Box
+          p={6}
+          sx={{
+            textAlign: "center",
+          }}
+        >
           fa2 tokens minted on akaSwap
         </Box>
         {!data && <div>A moment please...</div>}

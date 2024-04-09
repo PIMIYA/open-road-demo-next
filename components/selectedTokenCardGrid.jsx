2211c9f3b7ed12c70e77271cc21dd16952740349
19 <sorarize@gmail.com>
2024-04-09 15:39:35 +0800

    Adjust selectedTokenCardGrid

--- components/selectedTokenCardGrid.jsx
+++ components/selectedTokenCardGrid.jsx
@@ -1,24 +1,19 @@
 /* MUI */
-import Skeleton from "@mui/material/Skeleton";
-import Box from "@mui/material/Box";
 import { styled } from "@mui/material/styles";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Unstable_Grid2";
-/* MUI - card */
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+import { Box, Skeleton, Typography } from "@mui/material";
+import { CardMedia } from "@mui/material";
+import Grid from '@mui/material/Unstable_Grid2';
+
 /* NEXT */
 import Link from "next/link";
 
 /* Style Grid Item */
-const Item = styled(Paper)(({ theme }) => ({
-  textAlign: "center",
-  boxShadow: "none",
+const Item = styled(Box)(({ theme }) => ({
   background: "transparent",
+  "& a": {
+    textDecoration: "none",
+    color: "inherit",
+  }
 }));
 
 export default function SelectedTokenCardGrid({ data }) {
@@ -29,12 +24,10 @@ export default function SelectedTokenCardGrid({ data }) {
           Array.from(new Array(6)).map(
             (_, index) => (
               <Grid xs={4}>
-                <Item>
                   <Skeleton variant="rectangular" height={200} sx={{mb: 1}} />
                   <Skeleton variant="text" />
                   <Skeleton variant="text" />
                   <Skeleton variant="text" width={100} />
-                </Item>
               </Grid>
         ))}
         {data &&
@@ -48,57 +41,47 @@ export default function SelectedTokenCardGrid({ data }) {
               tags,
               contract,
             }) => (
-              <Grid key={tokenId} xs={4} sm={4} md={4}>
+              <Grid key={tokenId} xs={4}>
                 <Item>
                   <Link
                     href="/event/[contract]/[tokenId]"
                     as={`/event/${contract}/${tokenId}`}
                   >
-                    <Card sx={{ maxWidth: 380 }}>
-                      <CardMedia
-                        component="img"
-                        alt="thumbnail"
-                        height="140"
-                        image={`https://assets.akaswap.com/ipfs/${thumbnailUri.replace(
-                          "ipfs://",
-                          ""
-                        )}`}
-                      />
-                      <CardContent>
-                        <Typography
-                          gutterBottom
-                          variant="body2"
-                          component="div"
-                        >
-                          {name}
-                        </Typography>
-                        <Typography
-                          gutterBottom
-                          variant="body2"
-                          color="text.secondary"
-                        >
-                          Edition of {amount}
-                        </Typography>
-                        <Box sx={{ fontSize: "12px", color: "text.secondary" }}>
-                          {/* <Box>
-                              {creators &&
-                                creators.map((creator, index) => (
-                                  <Box key={index} ml={0}>
-                                    {creator}
-                                  </Box>
-                                ))}
-                            </Box> */}
-                          <Box
-                            sx={{
-                              overflow: "hidden",
-                              textOverflow: "ellipsis",
-                            }}
-                          >
-                            {creators[0]}
-                          </Box>
-                        </Box>
-                      </CardContent>
-                    </Card>
+                    <CardMedia
+                      component="img"
+                      alt="thumbnail"
+                      height="230"
+                      sx={{
+                        mb: 1,
+                      }}
+                      image={`https://assets.akaswap.com/ipfs/${thumbnailUri.replace(
+                        "ipfs://",
+                        ""
+                      )}`}
+                    />
+
+                    <Typography variant="cardTitle" component="h6" gutterBottom>
+                      {name}
+                    </Typography>
+                    <Typography variant="body2">
+                      Edition of {amount}
+                    </Typography>
+                    <Box>
+                      {/* <Box>
+                          {creators &&
+                            creators.map((creator, index) => (
+                              <Box key={index} ml={0}>
+                                {creator}
+                              </Box>
+                            ))}
+                        </Box> */}
+                      <Typography
+                        variant="body2"
+                        noWrap={true}
+                      >
+                        {creators[0]}
+                      </Typography>
+                    </Box>
                   </Link>
                 </Item>
               </Grid>
--- styles/theme.ts
+++ styles/theme.ts
@@ -1,5 +1,13 @@
 import { createTheme } from '@mui/material/styles';
 
+import { TypographyProps } from '@mui/material/Typography';
+
+declare module '@mui/material/Typography' {
+  interface TypographyPropsVariantOverrides {
+    cardTitle: true;
+  }
+}
+
 const theme = createTheme({
   components: {
     MuiCssBaseline: {
@@ -8,6 +16,19 @@ const theme = createTheme({
           overflowX: 'hidden',
         }
       }),
+    },
+    MuiTypography: {
+      variants: [
+        {
+          props: { variant: 'cardTitle' },
+          style: {
+            fontSize: '1.2rem',
+            lineHeight: '1.3',
+            fontWeight: 'bold',
+            // marginBottom: '.5rem',
+          },
+        },
+      ],
     },
   },
   palette: {

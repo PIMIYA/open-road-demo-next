6791ea67efabd3614fb67d151c777bfdf93fdf21
19 <sorarize@gmail.com>
2024-04-21 09:09:51 +0800

    fix: missing dummy data

--- pages/claimsToken/[contract]/[tokenId]/index.jsx
+++ pages/claimsToken/[contract]/[tokenId]/index.jsx
@@ -10,11 +10,21 @@ import SingleToken from "@/components/singleToken";
 /* Routing */
 import { useRouter } from "next/router";
 
+import { getRandomCreator, getRandomObjectType, getRandomPeriod, getRandomPlace } from "@/lib/dummy";
+
 export default function Id({ data }) {
   //   console.log(data);
   // const router = useRouter();
   // const query = router.query;
   // console.log(query);
+
+  // TODO: replace dummy data with real data
+  if (data && !data.objectType) {
+    data.creator = getRandomCreator();
+    data.objectType = getRandomObjectType();
+    data.eventDate = getRandomPeriod();
+    data.eventPlace = getRandomPlace();
+  }
 
   return <SingleToken data={data} />;
 }

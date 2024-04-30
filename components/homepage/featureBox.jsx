e708a53c7b6ee5a76bff3929ac537a9092aa901b
19 <sorarize@gmail.com>
2024-04-30 16:35:52 +0800

    feat: transform FeatureBox with mouse position

--- components/homepage/featureBox.jsx
+++ components/homepage/featureBox.jsx
@@ -9,20 +9,58 @@ import bg2 from "@/public/bubble2_bg.png";
 import bg3 from "@/public/bubble3_bg.png";
 import bg4 from "@/public/bubble4_bg.png";
 import { useTheme } from '@emotion/react';
+import { useEffect, useRef, useState } from 'react';
+import { xx } from './utils';
+import { useGlobalContext } from '@/contexts/GlobalContext';
 
 const bgs = [bg1, bg2, bg3, bg4];
 
 export default function FeatureBox({ bgIndex, title, description }) {
+  const { mousePosition, setMousePosition } = useGlobalContext();
   const theme = useTheme();
-  description = parse(description.replaceAll('在場證明', '<strong>在場證明</strong>'));
-  const bg = bgs[bgIndex];
+  const cardRef = useRef(null);
 
+  const bg = bgs[bgIndex];
   const maxWidthMap = {
     0: 400,
     1: 440,
     2: 500,
-    3: 600,
+    3: 550,
   };
+
+  const [transform, setTransform] = useState('');
+
+  const getTransform = ({x, y}) => {
+    const card = cardRef.current;
+    if (!card) return '';
+
+    const { left, top, width, height } = card.getBoundingClientRect();
+
+    let rotateX = (y - (top + height / 2)) / height * -30;
+    let rotateY = (x - (left + width / 2)) / width * 30;
+
+    // limit rotation
+    const maxRotate = 30;
+    rotateX = Math.min(maxRotate, Math.max(-maxRotate, rotateX));
+    rotateY = Math.min(maxRotate, Math.max(-maxRotate, rotateY));
+
+    return `perspective(1300px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateZ(${50 + bgIndex * 30}px)`;
+  }
+
+  useEffect(() => {
+    const handler = () => {
+      setTransform(getTransform(mousePosition));
+    }
+
+    window.addEventListener('mousemove', handler);
+    window.addEventListener('scroll', handler);
+    return () => {
+      window.removeEventListener('mousemove', handler);
+      window.removeEventListener('scroll', handler);
+    };
+  }, [mousePosition]);
+
+  description = parse(description.replaceAll('在場證明', '<strong>在場證明</strong>'));
 
   return (
     <Box
@@ -34,69 +72,77 @@ export default function FeatureBox({ bgIndex, title, description }) {
         },
         margin: '0 auto',
         mb: 8,
-        ':nth-of-type(2n)': {
+        ':nth-of-type(2n+1)': {
           transform: { lg: 'translateX(60%)'},
         },
-        ':nth-of-type(2n+1)': {
+        ':nth-of-type(2n)': {
           transform: { lg: 'translateX(-60%)'},
         },
-        ':nth-of-type(n + 3)': {
+        ':nth-of-type(n + 2)': {
           mt: { lg: -30 },
         },
       }}
     >
-      <FadeOnScroll onceonly>
-        <Box
-          sx={{
-            maxHeight: 300,
-            height: { xs: '45vw', md: '25vw' },
-            background: '#fff',
-            position: 'relative',
-            borderRadius: '4vw',
-            border: '1px solid #aaa',
-            margin: '0 auto',
-          }}
+      <Box
+        ref={cardRef}
+        sx={{
+          transform,
+          transition: 'transform .2s',
+        }}
         >
-        <Image
-          src={bg}
-          alt={title}
-          style={{
-            width: 'auto',
-            height: '100%',
-            position: 'absolute',
-            left: '50%',
-            transform: 'translateX(-50%)',
-            pointerEvents: 'none',
-          }}
-        />
-        </Box>
-      </FadeOnScroll>
-      <FadeOnScroll onceonly>
-        <Box
-          sx={{
-            px: { xs: 2, md: 6},
-            py: { xs: 2, md: 4 },
-          }}
-        >
-          <Typography
-            component='h2'
-            variant='h5'
+        <FadeOnScroll onceonly>
+          <Box
             sx={{
-              fontWeight: 'bold',
-              background: theme.palette.highlight.main,
-              display: 'inline-block',
-              mb: 2,
+              maxHeight: 300,
+              height: { xs: '45vw', md: '25vw' },
+              background: '#fff',
+              position: 'relative',
+              borderRadius: '4vw',
+              border: '1px solid #aaa',
+              margin: '0 auto',
             }}
           >
-            {title}
-          </Typography>
-          <Typography
-            fontSize={18}
+          <Image
+            src={bg}
+            alt={title}
+            style={{
+              width: 'auto',
+              height: '100%',
+              position: 'absolute',
+              left: '50%',
+              transform: 'translateX(-50%)',
+              pointerEvents: 'none',
+            }}
+          />
+          </Box>
+        </FadeOnScroll>
+        <FadeOnScroll onceonly>
+          <Box
+            sx={{
+              px: { xs: 2, md: 6},
+              py: { xs: 2, md: 4 },
+            }}
           >
-            {description}
-          </Typography>
-        </Box>
-      </FadeOnScroll>
+            <Typography
+              component='h2'
+              variant='h5'
+              sx={{
+                fontWeight: 'bold',
+                background: theme.palette.highlight.main,
+                display: 'inline-block',
+                mb: 2,
+              }}
+            >
+              {title}
+            </Typography>
+            <Typography
+              fontSize={18}
+            >
+              {description}
+            </Typography>
+          </Box>
+        </FadeOnScroll>
+      </Box>
     </Box>
   );
 }
--- contexts/GlobalContext.js
+++ contexts/GlobalContext.js
@@ -7,15 +7,28 @@ export const GlobalProvider = ({ children }) => {
   const router = useRouter();
 
   const [isLanded, setIsLanded] = useState(false);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     if (router.pathname !== '/') {
       setIsLanded(true);
     }
+
+    const handleMouseMove = (event) => {
+      const { clientX, clientY } = event;
+      setMousePosition({ x: clientX, y: clientY });
+    };
+
+    document.body.addEventListener('mousemove', handleMouseMove);
   }, [router.pathname]);
 
   return (
-    <GlobalContext.Provider value={{ isLanded, setIsLanded }}>
+    <GlobalContext.Provider value={{
+      isLanded,
+      setIsLanded,
+      mousePosition,
+      setMousePosition,
+    }}>
       {children}
     </GlobalContext.Provider>
   );

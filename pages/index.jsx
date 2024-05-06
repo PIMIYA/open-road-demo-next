bb0ac0015c98102ef1d82e6726e4524225421c77
19 <sorarize@gmail.com>
2024-05-06 17:06:31 +0800

    fix: homepage copywriting

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,6 +11,25 @@ import FadeOnScroll from "@/components/fadeOnScroll";
 import GeneralTokenCardGrid from "@/components/GeneralTokenCardGrid";
 import Marquee from "@/components/homepage/Marquee";
 
+const features = [
+  {
+    title: "紀錄你的藝文參與足跡！",
+    description: "每次參加現場活動，都能拿到在場證明。內容由創作者決定，可能是節目單、練習的側拍、主視覺或另外創造的作品等。",
+  },
+  {
+    title: "與創作者建立連結",
+    description: "透過在場證明，創作者將知道哪些人最常參與他們的活動，藉此獎勵自己的粉絲，建立會員。同時，持有在場證明者，可以對活動發表評論。"
+  },
+  {
+    title: "探索喜好　認識環境",
+    description: "透過在場證明，你能找到喜好相近的朋友；所有人可以一起看到當前藝術生態系的發展。",
+  },
+  {
+    title: "保有匿名性的開放",
+    description: "參與者保管個資，在場證明公開於鏈上。借助區塊鏈技術，建立安全、匿名但真實的開放資料。"
+  },
+];
+
 export default function Home({ data }) {
   const { isLanded } = useGlobalContext();
   const theme = useTheme();
@@ -22,10 +41,11 @@ export default function Home({ data }) {
       </Container>
         {isLanded &&
           <Container maxWidth="lg">
-            <FeatureBox bgIndex={0} title="紀錄你的藝文活動！" description="每次參加活動，你都能拿到一份屬於該活動的在場證明。內容由創作者決定，可能是節目單、可能是練習的側拍、可能是海報主視覺。" />
-            <FeatureBox bgIndex={1} title="與創作者建立連結" description="透過在場證明，創作者將知道誰最常參與過他們的活動。<br />同時，唯有持有在場證明者，可以對活動發表評論。" />
-            <FeatureBox bgIndex={2} title="探索喜好" description="透過在場證明，所有人皆可以探索彼此的藝文活動路徑。身為參與者，你可能找到跟你喜好相近的同好；身為創作者，你可能找到將來的合作對象。" />
-            <FeatureBox bgIndex={3} title="保有匿名性的開放" description="所有資料都公開於鏈上，所有 ID 都是錢包地址，<br />借助 web3 的技術達到安全、匿名但真實的開放資料。" />
+            {
+              features.map((feature, index) => (
+                <FeatureBox key={index} bgIndex={index} title={feature.title} description={feature.description} />
+              ))
+            }
           </Container>
         }
         {isLanded &&

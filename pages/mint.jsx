43dab87dcd2253d90bd4d7a8b3ad9ee639f799f3
wuianski <wuiyeh@gmail.com>
2024-05-31 01:15:00 +0800

    update

--- pages/mint.jsx
+++ pages/mint.jsx
@@ -736,7 +736,7 @@ export default function Mint() {
               ) : null}
             </Box>
             <Box>
-              {file && file.type.startsWith("image/") ? (
+              {(file && file.type.startsWith("image/")) || !file ? (
                 <></>
               ) : (
                 <Box>

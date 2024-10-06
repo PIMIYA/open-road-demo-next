c70c14364fc05558e4aad6cdc7fed6db0068da90
HsienYu Cheng <chenghsienyu@HsienYus-MacBook-Pro.local>
2024-10-07 01:18:02 +0800

    update the claimNFT page

--- components/KukaiEmbedComponent.jsx
+++ components/KukaiEmbedComponent.jsx
@@ -1,97 +1,177 @@
-import { useEffect, useRef, useState, forwardRef, useImperativeHandle } from 'react';
-import { KukaiEmbed, Networks } from 'kukai-embed';
+import {
+  useEffect,
+  useRef,
+  useState,
+  forwardRef,
+  useImperativeHandle,
+} from "react";
+import { KukaiEmbed, Networks } from "kukai-embed";
+import { Button } from "@mui/material";
 
-const KukaiEmbedComponent = forwardRef(({ onLoginSuccess }, ref) =>  {
-    const embedRef = useRef(null);
-    const [isEmbedOpen, setIsEmbedOpen] = useState(false);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+const KukaiEmbedComponent = forwardRef(({ onLoginSuccess }, ref) => {
+  const embedRef = useRef(null);
+  const [isEmbedOpen, setIsEmbedOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    useImperativeHandle(ref, () => ({
-        logout: async () => {
-            if (embedRef.current) {
-                try {
-                    await embedRef.current.logout();
-                    setIsLoggedIn(false);
-                    console.log('Logged out successfully');
-                } catch (error) {
-                    console.error('Error during logout:', error);
-                }
-            }
+  useImperativeHandle(ref, () => ({
+    logout: async () => {
+      if (embedRef.current) {
+        try {
+          await embedRef.current.logout();
+          setIsLoggedIn(false);
+          console.log("Logged out successfully");
+        } catch (error) {
+          console.error("Error during logout:", error);
         }
-    }));
+      }
+    },
+  }));
 
-    const initializeEmbed = () => {
-        if (typeof window !== 'undefined' && typeof document !== 'undefined') {
-            if (embedRef.current) {
-                console.log('Kukai-Embed already initialized');
-                return;
-            }
-            const embed = new KukaiEmbed({
-                net: Networks.ghostnet,
-                element: document.getElementById('kukai-container')
-            });
 
-            embed.init().then(() => {
-                if (embed.user) {
-                    console.log('Already logged in', embed.user);
-                    setIsLoggedIn(true);
-                }
-            }).catch(error => {
-                console.error('Error during initialization:', error);
-            });
+  const initializeEmbed = () => {
+    if (typeof window !== "undefined" && typeof document !== "undefined") {
+      if (embedRef.current) {
+        console.log("Kukai-Embed already initialized");
+        return;
+      }
+      const embed = new KukaiEmbed({
+        net: Networks.ghostnet,
+        element: document.getElementById("kukai-container"),
+      });
 
-            embedRef.current = embed;
+      embed
+        .init()
+        .then(() => {
+          if (embed.user) {
+            console.log("Already logged in", embed.user);
+            setIsLoggedIn(true);
+          }
+
+        //   // Inject CSS into the iframe, if needed , this is optional and test it after KUKAI add kairos to whitelist
+
+        //   const iframe = document.getElementById("kukai-iframe");
+        //   if (iframe) {
+        //       const injectCSS = () => {
+        //           try {
+        //               const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+        //               const css = `
+        //                   .flex-row.header .abort {
+        //                       display: none !important;
+        //                   }
+        //                   .flex-row.header .back {
+        //                       display: none !important;
+        //                   }
+        //                   .footer {
+        //                       display: none !important;
+        //                   }
+        //                   body {
+        //                       background-color: red !important;
+        //                   }
+        //               `;
+        //               const style = iframeDocument.createElement("style");
+        //               style.appendChild(iframeDocument.createTextNode(css));
+        //               iframeDocument.head.appendChild(style);
+        //           } catch (error) {
+        //               console.error("Error injecting CSS:", error);
+        //           }
+        //       };
+  
+        //       if (iframe.contentDocument.readyState === "complete") {
+        //           injectCSS();
+        //       } else {
+        //           iframe.onload = injectCSS;
+        //       }
+        //     }
+        })
+        .catch((error) => {
+          console.error("Error during initialization:", error);
+        });
+
+      embedRef.current = embed;
+    }
+  };
+
+  useEffect(() => {
+    initializeEmbed();
+  }, []);
+
+  const handleLogin = async () => {
+    if (embedRef.current) {
+      try {
+        const userInfo = await embedRef.current.login({
+          loginOptions: ["google"],
+          wideButtons: [true],
+          showBackButton: [true],
+        });
+
+        if (userInfo) {
+          // console.log(`userInfo is : ${JSON.stringify(userInfo)}`);
+          setIsLoggedIn(true);
+          onLoginSuccess(userInfo);
         }
-    };
+      } catch (error) {
+        console.error("Error during login:", error);
+      }
+    }
+    setIsEmbedOpen(true);
+  };
 
-    useEffect(() => {
-        initializeEmbed();
-    }, []);
+  // const handleLogout = async () => {
+  //     if (embedRef.current) {
+  //         try {
+  //             await embedRef.current.logout();
+  //             setIsLoggedIn(false);
+  //             console.log('Logged out successfully');
+  //         } catch (error) {
+  //             console.error('Error during logout:', error);
+  //         }
+  //     }
+  // };
 
-    const handleLogin = async () => {
-        if (embedRef.current) {
-            try {
-                const userInfo = await embedRef.current.login({
-                    loginOptions: ['google'],
-                    wideButtons: [true],
-                    showBackButton: [true],
-                });
-
-                if (userInfo) {
-                    // console.log(`userInfo is : ${JSON.stringify(userInfo)}`);
-                    setIsLoggedIn(true);
-                    onLoginSuccess(userInfo);
-                }
-            } catch (error) {
-                console.error('Error during login:', error);
-            }
-        }
-        setIsEmbedOpen(true);
-    };
-
-    // const handleLogout = async () => {
-    //     if (embedRef.current) {
-    //         try {
-    //             await embedRef.current.logout();
-    //             setIsLoggedIn(false);
-    //             console.log('Logged out successfully');
-    //         } catch (error) {
-    //             console.error('Error during logout:', error);
-    //         }
-    //     }
-    // };
-
-    return (
-        <div>
-            {isLoggedIn ? (
-                <button onClick={() => ref.current.logout()}>Logout</button>
-            ) : (
-                <button onClick={handleLogin}>click to claimNFT</button>
-            )}
-            {isEmbedOpen && <div id="kukai-container"></div>}
-        </div>
-    );
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        // marginBottom: "16px",
+        marginTop: "16px",
+      }}
+    >
+      {isLoggedIn ? (
+        <Button
+          onClick={() => ref.current.logout()}
+          sx={{
+            backgroundColor: "#007bff",
+            color: "#fff",
+            padding: "8px 16px",
+            borderRadius: "4px",
+            "&:hover": {
+              backgroundColor: "#0056b3",
+            },
+          }}
+        >
+          Logout
+        </Button>
+      ) : (
+        <Button
+          onClick={handleLogin}
+          sx={{
+            backgroundColor: "#007bff",
+            color: "#fff",
+            padding: "8px 16px",
+            borderRadius: "4px",
+            "&:hover": {
+              backgroundColor: "#0056b3",
+            },
+          }}
+        >
+          click to claim NFT
+        </Button>
+      )}
+      {isEmbedOpen && <div id="kukai-container"></div>}
+    </div>
+  );
 });
 
 export default KukaiEmbedComponent;
-
---
+++ components/NFTclaim/index.jsx
@@ -0,0 +1,183 @@
+import { useTheme } from "@emotion/react";
+import { useRouter } from "next/router";
+
+import Link from "next/link";
+import Image from "next/image";
+import {
+  Box,
+  Button,
+  Container,
+  Stack,
+  Paper,
+  Typography,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+
+import Tags from "@/components/Tags";
+import TokenCollectors from "../singleToken/TokenCollectors";
+import TokenClaimedProgress from "../singleToken/TokenClaimedProgress";
+import RenderMedia from "@/components/render-media";
+
+import { getAkaswapAssetUrl } from "@/lib/stringUtils";
+import { encrypt } from "@/lib/dummy";
+
+/* stack Item setting */
+const Item = styled(Paper)(({ theme }) => ({
+  textAlign: "left",
+  boxShadow: "none",
+}));
+
+export default function NFTclaim({ ownersData, data }) {
+  const router = useRouter();
+  const theme = useTheme();
+  const tokenImageUrl = getAkaswapAssetUrl(data.metadata.displayUri);
+  const total = ownersData.amount;
+  const collected = ownersData ? Object.keys(ownersData.owners).length - 2 : 0;
+  const ownerAddresses = ownersData ? Object.keys(ownersData.owners) : 0;
+
+  const url = `${router.query.contract}/${router.query.tokenId}`;
+  const hash = encrypt(url);
+  const showcaseUrl = `/showcase/${hash}`;
+  const isShowcasePageLinkAvailable = true; // TODO: depends on wallet
+
+  const mimeType = data.metadata.formats[0].mimeType;
+  const src = data.metadata;
+  const poolId = data.poolId;
+  const duration = data.duration;
+  // console.log(mimeType);
+  // console.log(duration);
+
+  return (
+    <>
+      <Box sx={{ background: "#fff" }} mx={3} borderRadius={2}>
+        <Container maxWidth="lg">
+          <Box py={6}>
+            <Stack direction="column" spacing={8}>
+              <Item
+                sx={{
+                  width: "100%",
+                  height: "auto",
+                }}
+              >
+                <Box
+                  sx={{
+                    width: "100%",
+                    height: "auto",
+                  }}
+                >
+                  <RenderMedia mimeType={mimeType} src={src} />
+                </Box>
+              </Item>
+              <Item
+                sx={{
+                  width: "100%",
+                  height: "auto",
+                }}
+              >
+                <Box
+                  sx={{
+                    width: "100%",
+                    height: "auto",
+                  }}
+                >
+                  <Box>
+                    <Box mb={2} width="100%">
+                      {collected !== 0 ? (
+                        <TokenClaimedProgress
+                          collected={collected}
+                          total={total}
+                        />
+                      ) : (
+                        <Typography variant="h6" component="h1">
+                          No Owner
+                        </Typography>
+                      )}
+                    </Box>
+
+                    <Typography variant="h4" component="h1">
+                      {data.metadata.name}
+                    </Typography>
+                    <Typography variant="h6" component="div" mb={2}>
+                      {data.creator}
+                    </Typography>
+
+                    <Typography variant="body2">
+                      {data.start_time
+                        ? new Date(data.start_time).toLocaleDateString() +
+                          " - " +
+                          new Date(data.end_time).toLocaleDateString()
+                        : eventDate}
+                    </Typography>
+                    <Typography mb={2}>{data.eventPlace}</Typography>
+
+                    {data.tags && (
+                      <Box mb={8}>
+                        <Tags tags={data.tags} />
+                      </Box>
+                    )}
+
+                    <Box>
+                      {data.metadata.description
+                        .split("\n")
+                        .map((paragraph, index) => (
+                          <Typography key={index} paragraph>
+                            {paragraph}
+                          </Typography>
+                        ))}
+                    </Box>
+
+                    {/* add poolID for claim page and event time from akadrop  */}
+                    <Box
+                      sx={{
+                        color: "#000",
+                        padding: "16px",
+                        borderRadius: "8px",
+                        marginBottom: "16px",
+                      }}
+                    >
+                      {poolId !== null ? (
+                        <>
+                          <Typography variant="h6">
+                            Pool ID: {poolId}
+                          </Typography>
+                          <Box
+                            sx={{
+                              display: "flex",
+                              justifyContent: "space-between",
+                              alignItems: "center",
+                            }}
+                          >
+                            <Typography variant="body2">
+                              Start Time: {duration.start_time}
+                            </Typography>
+                            <Typography variant="body2">
+                              End Time: {duration.end_time}
+                            </Typography>
+                          </Box>
+                        </>
+                      ) : (
+                        <Typography variant="body2">
+                          Expired or not able to claim
+                        </Typography>
+                      )}
+                    </Box>
+                    {/*  */}
+                  </Box>
+                </Box>
+              </Item>
+            </Stack>
+          </Box>
+        </Container>
+      </Box>
+
+      {/* {ownerAddresses === 0 ? null : (
+        <TokenCollectors
+          owners={ownersData.owners}
+          ownerAddresses={ownerAddresses}
+          ownerAliases={ownersData.ownerAliases}
+        />
+      )} */}
+    </>
+  );
+}
